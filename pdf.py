b0051572044d0a916cd0095f2f90c0f29bd3c2ab
Timurr32 <99083657@mydavinci.nl>
2025-01-29 11:28:59 +0100

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,18 +1,65 @@
 import os
 from fpdf import FPDF
 
+class PDF(FPDF):
+    def header(self):
+        self.set_font('helvetica', 'B', 12)
+        self.cell(0, 10, 'XYZ Services B.V.', ln=True, align='L')
+        self.set_font('helvetica', '', 10)
+        self.cell(0, 10, '123, 1234 AB Amsterdam', ln=True, align='L')
+        self.cell(0, 10, 'Postcode: 1234 AB', ln=True, align='L')
+        self.cell(0, 10, 'BTW-nummer: NL123456789B01', ln=True, align='L')
+        self.ln(10)
+
+    def footer(self):
+        
+        self.set_y(-15)
+        self.set_font('helvetica', 'I', 8)
+        self.cell(0, 10, f'Pagina {self.page_no()}', 0, 0, 'C')
+
+    def add_factuurregels(self):
+        self.set_font('helvetica', '', 12)
+        
+        
+        self.cell(50, 10, 'Omschrijving', border=1)
+        self.cell(30, 10, 'Aantal', border=1)
+        self.cell(30, 10, 'Prijs', border=1)
+        self.cell(30, 10, 'Totaal', border=1)
+        self.ln(10)
+        
+        
+        self.cell(50, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.ln(10)
+        
+        
+        self.cell(50, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.ln(10)
+        
+        
+        self.cell(50, 10, 'Totaal', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.cell(30, 10, '', border=1)
+        self.ln(10)
+
 
 map_pad = 'PDF_INVOICE'
 if not os.path.exists(map_pad):
     os.makedirs(map_pad)
 
-pdf = FPDF('P', 'mm', 'letter')
+
+pdf = PDF('P', 'mm', 'letter')
 pdf.add_page()
-pdf.set_font('helvetica', '', 16)
-
-gebruiker = input("Voer de tekst in die je aan de PDF wilt toevoegen: ")
-pdf.cell(40, 10, gebruiker)
 
 
-pdf_output_pad = os.path.join(map_pad, 'opdrachttt3.pdf')
+pdf.add_factuurregels()
+
+
+pdf_output_pad = os.path.join(map_pad, 'jouw_factuur.pdf')
 pdf.output(pdf_output_pad)
